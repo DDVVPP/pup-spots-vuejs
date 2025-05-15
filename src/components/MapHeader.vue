394a20eb63540fca420aa86afd09704d81008bc4
DDVVPP <darshinvp@gmail.com>
2025-05-15 11:50:43 -0700

    feat: added MapHeader component

---
+++ src/components/MapHeader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header
+    role="banner"
+    className="absolute top-10 left-10 max-sm:right-10 z-40 w-fit bg-white/85 rounded-xl border-brand-corral border shadow-md"
+  >
+    <div className="p-4">
+      <h1
+        className="text-xl text-brand-corral font-semibold flex items-center gap-x-2"
+      >
+        Pup Spots
+        <!-- <PawPrint size="{24}" color="#f87171" weight="duotone" /> -->
+      </h1>
+      <p className="text-xs text-slate-700 mt-1">
+        A map of dog-friendly hangouts across LA
+      </p>
+    </div>
+  </header>
+</template>
