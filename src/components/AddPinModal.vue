9e57d537ebcf1a0ae71640f73b7bdc40dfdecf9c
DDVVPP <darshinvp@gmail.com>
2025-05-14 19:10:26 -0700

    wip: initialized the AddPinModal component

---
+++ src/components/AddPinModal.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="fixed inset-0 z-50 flex items-center justify-center bg-black/5 backdrop-blur-sm"
+  >
+    <div
+      class="bg-white rounded-lg p-6 shadow-md max-w-md w-full flex-col flex gap-y-2"
+    >
+      <h1 class="text-lg font-semibold mb-2">Add Pup Spot</h1>
+      <p class="text-sm text-gray-600">
+        Add a title, description, and category for this pin..
+      </p>
+      <label for="title">Title</label>
+      <input placeholder="Elysian Park" class="border p-2 rounded-md" />
+      <label for="notes">Notes</label>
+      <input
+        placeholder="Great views, green, coyotes around"
+        class="border p-2 rounded-md"
+      />
+
+      <label for="categories">Choose a category:</label>
+      <select name="categories" id="categories" class="border p-2 rounded-md">
+        <option value="restaurants">Restaurant</option>
+        <option value="bars">Bar</option>
+        <option value="hikes">Hike</option>
+        <option value="shops">Shop</option>
+        <option value="parks">Park</option>
+        <option value="cafes">Cafe</option>
+      </select>
+      <button class="bg-red-300 rounded-md">Add Pin</button>
+      <button class="border-red-500 border rounded-md">Cancel</button>
+    </div>
+  </div>
+</template>
--- src/components/MapboxMap.vue
+++ src/components/MapboxMap.vue
@@ -2,25 +2,41 @@
 import { onMounted, ref } from "vue";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import AddPinModal from "./AddPinModal.vue";
 
 const mapboxToken = import.meta.env.VITE_MAPBOX_TOKEN;
 const mapContainer = ref<HTMLElement | null>(null);
+const map = ref();
+
+const selectedCoords = ref<{
+  lng: number;
+  lat: number;
+}>();
+const isOpen = ref(false);
 
 onMounted(() => {
   mapboxgl.accessToken = mapboxToken;
 
   if (!mapContainer.value) return;
 
-  new mapboxgl.Map({
+  map.value = new mapboxgl.Map({
     container: mapContainer.value,
     style: "mapbox://styles/dvanparijs/cm9rywpj900ht01rmfnwk0y30",
     center: [-118.2437, 34.0522], // [lng, lat] → Los Angeles
     zoom: 11,
   });
+
+  map.value.on("click", (e) => {
+    selectedCoords.value = e.lngLat;
+    isOpen.value = true;
+  });
 });
 </script>
 
 <template>
+  <div v-if="isOpen">
+    <AddPinModal />
+  </div>
   <section
     aria-label="Interactive map of dog-friendly spots"
     ref="mapContainer"
