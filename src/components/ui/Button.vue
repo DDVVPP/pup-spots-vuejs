83475a29afe3bfd3cfacb67e8ed29ce5c6587c47
DDVVPP <darshinvp@gmail.com>
2025-05-20 10:04:13 -0700

    style: updated Button component styles

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -15,21 +15,22 @@ withDefaults(
 
 const variantClassMap = {
   primary: `
-    bg-red-400
-    hover:bg-red-500
+    bg-brand-corral
+    border-none
     text-white
   `,
   secondary: `
+    bg-white
+    border-pink-950/30
+    hover:bg-white
     text-slate-700
-    border-slate-300
-    hover:bg-slate-200
   `,
 };
 </script>
 
 <template>
   <button
-    class="rounded-md border px-4 py-2 transition disabled:opacity-50 disabled:cursor-not-allowed"
+    class="rounded-md border px-4 py-2 disabled:opacity-50 disabled:cursor-not-allowed hover:shadow-none hover:translate-x-[2px] hover:translate-y-[2px] shadow-[2px_2px_rgba(131,24,67,1)]"
     :class="[variantClassMap[variant]]"
   >
     {{ text }}
