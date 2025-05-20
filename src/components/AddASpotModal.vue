f6790661067f08f9b04aabb6e4f5dac3678d8a80
DDVVPP <darshinvp@gmail.com>
2025-05-20 10:08:39 -0700

    refactor: added Button component and moved styles to scoped styles

--- src/components/AddASpotModal.vue
+++ src/components/AddASpotModal.vue
@@ -6,12 +6,8 @@ const emit = defineEmits(["toggleIsOpen"]);
 </script>
 
 <template>
-  <div
-    class="fixed inset-0 z-50 flex items-center justify-center bg-black/5 backdrop-blur-sm"
-  >
-    <div
-      class="bg-white rounded-lg p-6 shadow-md max-w-md w-full flex-col flex gap-y-2"
-    >
+  <div class="modal-wrapper">
+    <div class="modal">
       <h1 class="text-lg font-semibold mb-2">Add Pup Spot</h1>
       <p class="text-sm text-gray-600">
         Add a title, description, and category for this pin..
@@ -22,24 +18,32 @@ const emit = defineEmits(["toggleIsOpen"]);
         label="Choose a category"
         :options="['restaurants', 'bars', 'hikes', 'shops', 'parks', 'cafes']"
       />
-      <Button text="Add Pin" />
-      <Button text="Cancel" variant="secondary" />
 
-      <button class="add-a-spot-button" @click="emit('toggleIsOpen')">
-        Submit for Approval
-      </button>
-      <button class="add-a-spot-button" @click="emit('toggleIsOpen')">
-        Cancel
-      </button>
+      <div class="buttons-wrapper">
+        <Button
+          text="Submit for Approval"
+          @click="emit('toggleIsOpen')"
+          class="w-1/2"
+        />
+        <Button
+          text="Cancel"
+          variant="secondary"
+          @click="emit('toggleIsOpen')"
+          class="w-1/2"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.add-a-spot-button-wrapper {
-  @apply absolute top-20 right-7 z-10 w-fit max-w-md max-sm:inset-x-6 max-sm:top-[140px] max-sm:mx-auto;
+.modal-wrapper {
+  @apply fixed inset-0 z-50 flex items-center justify-center bg-black/5 backdrop-blur-sm;
 }
-.add-a-spot-button {
-  @apply font-medium p-3 px-4 leading-none rounded-xl border-pink-950 text-white bg-brand-corral shadow-[4px_4px_rgba(131,24,67,1)] hover:shadow-none hover:translate-x-[4px] hover:translate-y-[4px];
+.modal {
+  @apply bg-white rounded-lg p-6 max-w-md w-full flex-col flex gap-y-2 shadow-lg border border-pink-950/15;
+}
+.buttons-wrapper {
+  @apply flex justify-between mt-2 gap-x-4 mr-1;
 }
 </style>
