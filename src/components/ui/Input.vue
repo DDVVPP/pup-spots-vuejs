effd31d84b62e98cd8ffe27d2acf2ba182cf0082
DDVVPP <darshinvp@gmail.com>
2025-05-20 13:30:30 -0700

    style: updated Select and Input components with smaller screen styles

--- src/components/ui/Input.vue
+++ src/components/ui/Input.vue
@@ -6,11 +6,15 @@ defineProps<{
 </script>
 
 <template>
-  <label :for="label" class="text-sm font-medium text-slate-700">{{
-    label
-  }}</label>
-  <input
-    :placeholder="placeholder"
-    class="placeholder:text-slate-300 text-xs text-slate-600 rounded-md border p-2"
-  />
+  <div class="flex flex-col gap-y-1">
+    <label
+      :for="label"
+      class="text-sm max-sm-smaller:text-xs font-medium text-slate-700"
+      >{{ label }}</label
+    >
+    <input
+      :placeholder="placeholder"
+      class="placeholder:text-slate-300 text-xs text-slate-600 rounded-md border p-2"
+    />
+  </div>
 </template>
--- src/components/ui/Select.vue
+++ src/components/ui/Select.vue
@@ -6,21 +6,26 @@ defineProps<{
 </script>
 
 <template>
-  <label :for="label" class="block text-sm font-medium text-slate-700 mb-1">
-    {{ label }}
-  </label>
-  <select
-    :name="label"
-    :id="label"
-    class="border p-2 rounded-md w-full text-sm text-slate-700"
-  >
-    <option
-      v-for="option in options"
-      :key="option"
-      :value="option"
-      class="text-xs"
+  <div class="flex flex-col gap-y-1">
+    <label
+      :for="label"
+      class="block text-sm max-sm-smaller:text-xs font-medium text-slate-700"
     >
-      {{ option }}
-    </option>
-  </select>
+      {{ label }}
+    </label>
+    <select
+      :name="label"
+      :id="label"
+      class="border p-2 rounded-md w-full text-xs text-slate-700"
+    >
+      <option
+        v-for="option in options"
+        :key="option"
+        :value="option"
+        class="text-xs"
+      >
+        {{ option }}
+      </option>
+    </select>
+  </div>
 </template>
