166efcd1a08ddb7d8f4ede3eb5c2a4c65b39f58c
DDVVPP <darshinvp@gmail.com>
2025-05-26 10:34:50 -0700

    feat: added a TextareaInput component

---
+++ src/components/ui/TextareaInput.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+defineProps<{
+  label: string;
+  placeholder: string;
+  modelValue?: string | null;
+  errorMessage?: string;
+  isRequired?: boolean;
+  rows?: number;
+}>();
+
+defineEmits<{
+  "update:modelValue": [value: string | null];
+}>();
+</script>
+
+<template>
+  <div class="flex flex-col gap-y-1">
+    <label
+      :for="label"
+      class="text-sm max-sm-smaller:text-xs font-medium text-slate-700"
+      >{{ label
+      }}<span v-if="isRequired" class="font-normal"> (required)</span></label
+    >
+    <textarea
+      :id="label"
+      :placeholder="placeholder"
+      class="placeholder:text-slate-300 text-xs text-slate-600 rounded-md border p-2"
+      @input="
+        $emit('update:modelValue', ($event.target as HTMLInputElement).value)
+      "
+      :value="modelValue"
+      :rows="rows"
+    />
+    <p v-if="errorMessage" class="text-red-500 text-sm max-sm-smaller:text-xs">
+      {{ errorMessage }}
+    </p>
+  </div>
+</template>
