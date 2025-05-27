79fb0e88a142ea3fd43e5ff64a4fd0f874a3584e
DDVVPP <darshinvp@gmail.com>
2025-05-26 19:06:33 -0700

    refactor: added a FieldError component to avoid repetition in Input components

---
+++ src/components/ui/FieldError.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  message?: string;
+}>();
+</script>
+
+<template>
+  <p
+    class="error-message"
+    :class="{
+      'opacity-100': message,
+      'opacity-0': !message,
+    }"
+  >
+    {{ message ?? " " }}
+  </p>
+</template>
+
+<style scoped>
+.error-message {
+  @apply text-brand-corral text-sm max-sm-smaller:text-xs transition-opacity duration-200 ease-in min-h-5;
+}
+</style>
